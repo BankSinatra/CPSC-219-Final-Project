50f07929912f11dfc050bed8bc41ee4782aa6dcf
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-11-27 22:08:55 +0000

    Populated the laundry pal home controller

--- src/application/LaundryPalHomeController.java
+++ src/application/LaundryPalHomeController.java
@@ -1,5 +1,128 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LaundryPalHomeController {
+
+    @FXML
+    private Button btn_wash_30;
+
+    @FXML
+    private Button btn_wash_50;
+
+    @FXML
+    private Button btn_hcs_gentle;
+
+    @FXML
+    private Button btn_tumble_dry_low;
+
+    @FXML
+    private Button btn_dry;
+
+    @FXML
+    private Button btn_drip_dryshade;
+
+    @FXML
+    private Button btn_wash;
+
+    @FXML
+    private Button btn_no_chemical_clean;
+
+    @FXML
+    private Button btn_dry_shade;
+
+    @FXML
+    private Button btn_pce_only;
+
+    @FXML
+    private Button btn_line_dry;
+
+    @FXML
+    private Button btn_hcs_very_gentle;
+
+    @FXML
+    private Button btn_dry_flat_shade;
+
+    @FXML
+    private Button btn_pro_cleaning;
+
+    @FXML
+    private Button btn_hand_wash;
+
+    @FXML
+    private Button btn_no_wet_clean;
+
+    @FXML
+    private Button btn_do_not_wash;
+
+    @FXML
+    private Button btn_do_not_bleach;
+
+    @FXML
+    private Button btn_wash_40;
+
+    @FXML
+    private Button btn_pce_very_gentle;
+
+    @FXML
+    private Button btn_wet_clean_gentle;
+
+    @FXML
+    private Button btn_bleach;
+
+    @FXML
+    private Button btn_wet_clean;
+
+    @FXML
+    private Button btn_dry_flat;
+
+    @FXML
+    private Button btn_wet_clean_very_gentle;
+
+    @FXML
+    private Button btn_bleach_ch;
+
+    @FXML
+    private Button btn_wash_60;
+
+    @FXML
+    private Button btn_do_not_tumble_dry;
+
+    @FXML
+    private Button btn_iron;
+
+    @FXML
+    private Button btn_line_dry_shade;
+
+    @FXML
+    private Button btn_tumble_dry;
+
+    @FXML
+    private Button btn_iron_high;
+
+    @FXML
+    private Button btn_drip_dry;
+
+    @FXML
+    private Button btn_hcs_only;
+
+    @FXML
+    private Button btn_pce_gentle;
+
+    @FXML
+    private Button btn_tumble_dry_normal;
+
+    @FXML
+    private Button btn_do_not_iron;
+
+    @FXML
+    private Button btn_iron_low;
+
+    @FXML
+    private Button btn_bleach_no_ch;
+
+    @FXML
+    private Button btn_iron_medium;
 
 }
