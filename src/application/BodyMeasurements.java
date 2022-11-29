9351d5bc17a4e69fa0dd2cba915c90863cbfde34
ShannonPett <shannon.pett@ucalgary.ca>
2022-11-29 15:15:09 -0700

    getter and setter for head circumference.

--- src/application/BodyMeasurements.java
+++ src/application/BodyMeasurements.java
@@ -1,9 +1,29 @@
 package application;
 
 public class BodyMeasurements {
+	
+	private int headCircumference =0;
 
 	public BodyMeasurements() {
 		// TODO Auto-generated constructor stub
 	}
+
+	int getHeadCircumference() {
+		return headCircumference;
+	}
+
+	void setHeadCircumference(int headCircumference) {
+		if (headCircumference <= 65 && headCircumference >= 54){
+			this.headCircumference = headCircumference;
+		}
+	}
+	
+	// need measurements for: US SIZING
+	// hat - need head circumference. small: 54-55cm medium: 56-57cm large: 58-59cm extra large: 60-61cm xxlarge: 62-63cm xxxlarge: 64-65cm
+
+	// sweaters
+	// jacket
+	// bottoms
+	// tops 
 
 }
