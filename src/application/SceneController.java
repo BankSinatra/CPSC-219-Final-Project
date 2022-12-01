bc48ffc9fe3dadde2055f0c2e6b96d3701519bf2
ShannonPett <shannon.pett@ucalgary.ca>
2022-11-30 23:50:53 -0700

    Can switch between home screen and clothing choice scene.

--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -46,4 +46,12 @@ public class SceneController {
 	 stage.setScene(scene);
 	 stage.show();
  }
+ 
+ public void switchToLaundryPalScene (ActionEvent event) throws IOException {
+	 Parent root = FXMLLoader.load(getClass().getResource("LaundryPalHome.fxml"));
+	 stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	 scene = new Scene(root);
+	 stage.setScene(scene);
+	 stage.show();
+ }
 }
