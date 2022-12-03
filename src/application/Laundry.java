1e5df908db6f43cb1e2ad5636ed2dda5077bf151
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-03 16:55:26 -0700

    Fully implemented laundry class. Removed LaundrySymbol class

--- src/application/Laundry.java
+++ src/application/Laundry.java
@@ -4,5 +4,23 @@ import java.util.ArrayList;
 
 public abstract class Laundry {
     private LaundryMethod laundryMethod;
-    private ArrayList<String> instructions;
+    private String instructions;
+
+    Laundry(LaundryMethod laundryMethod){
+        this.laundryMethod = laundryMethod;
+    }
+
+    public LaundryMethod getLaundryMethod() {
+        return laundryMethod;
+    }
+    public void setLaundryMethod(LaundryMethod laundryMethod){
+        this.laundryMethod = laundryMethod;
+    }
+    public String getInstructions(){
+        return instructions;
+    }
+
+    public void setInstructions(String instructions){
+        this.instructions = instructions;
+    }
 }
