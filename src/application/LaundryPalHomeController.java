9aafe91720f1e2170e4e9aba4bbed98337dae83d
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-03 20:17:34 -0700

    Changed bind image function to take the location of the image resource

--- src/application/LaundryPalHomeController.java
+++ src/application/LaundryPalHomeController.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -156,7 +157,8 @@ public class LaundryPalHomeController {
 		stage.show();
 		}
     
-	    private void bindImage(FileInputStream file, Button btn, String instructions){
+	    private void bindImage(String location, Button btn, String instructions) throws FileNotFoundException {
+			FileInputStream file = new FileInputStream(location);
 	        Image img = new Image(file);
 	        ImageView imageView = new ImageView(img);
 	        imageView.setPreserveRatio(true);
@@ -193,59 +195,59 @@ public class LaundryPalHomeController {
 	    public void initialize() {
 	        try{
 				//Washing
-	            bindImage(new FileInputStream("Resources/LaundrySymbols/Washing/Wash.svg.png"), btn_wash, "Wash your article of clothing in water");
-	            bindImage(new FileInputStream("Resources/LaundrySymbols/Washing/Wash_30.svg.png"), btn_wash_30, "Wash your clothing in water at or below 30 °C");
-	            bindImage(new FileInputStream("Resources/LaundrySymbols/Washing/Wash_40.svg.png"), btn_wash_40, "Wash your clothing in water at or below 40 °C");
-	            bindImage(new FileInputStream("Resources/LaundrySymbols/Washing/Wash_50.svg.png"), btn_wash_50, "Wash your clothing in water at or below 50 °C");
-	            bindImage(new FileInputStream("Resources/LaundrySymbols/Washing/Wash_60.svg.png"), btn_wash_60, "Wash your clothing in water at or below 60 °C");
-	            bindImage(new FileInputStream("Resources/LaundrySymbols/Washing/Handwash.svg.png"), btn_hand_wash, "Hand wash your clothing");
-	            bindImage(new FileInputStream("Resources/LaundrySymbols/Washing/No_Wash.svg.png"), btn_do_not_wash, "Do not wash your clothing in water");
+	            bindImage("Resources/LaundrySymbols/Washing/Wash.svg.png", btn_wash, "Wash your article of clothing in water");
+	            bindImage("Resources/LaundrySymbols/Washing/Wash_30.svg.png", btn_wash_30, "Wash your clothing in water at or below 30 °C");
+	            bindImage("Resources/LaundrySymbols/Washing/Wash_40.svg.png", btn_wash_40, "Wash your clothing in water at or below 40 °C");
+	            bindImage("Resources/LaundrySymbols/Washing/Wash_50.svg.png", btn_wash_50, "Wash your clothing in water at or below 50 °C");
+	            bindImage("Resources/LaundrySymbols/Washing/Wash_60.svg.png", btn_wash_60, "Wash your clothing in water at or below 60 °C");
+	            bindImage("Resources/LaundrySymbols/Washing/Handwash.svg.png", btn_hand_wash, "Hand wash your clothing");
+	            bindImage("Resources/LaundrySymbols/Washing/No_Wash.svg.png", btn_do_not_wash, "Do not wash your clothing in water");
 
 				//Tumble Dry
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Tumble/Tumble_Dry.svg.png"), btn_tumble_dry, "You can machine dry your clothing");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Tumble/Tumble_Dry_Low.svg.png"), btn_tumble_dry_low, "Machine dry your clothing at a low temperature");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Tumble/Tumble_Dry_High.svg.png"), btn_tumble_dry_normal, "Machine dry your clothing at a normal temperature");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Tumble/No_Tumble_Dry.svg.png"), btn_do_not_tumble_dry, "Do not machine dry your clothing");
+				bindImage("Resources/LaundrySymbols/Drying/Tumble/Tumble_Dry.svg.png", btn_tumble_dry, "You can machine dry your clothing");
+				bindImage("Resources/LaundrySymbols/Drying/Tumble/Tumble_Dry_Low.svg.png", btn_tumble_dry_low, "Machine dry your clothing at a low temperature");
+				bindImage("Resources/LaundrySymbols/Drying/Tumble/Tumble_Dry_High.svg.png", btn_tumble_dry_normal, "Machine dry your clothing at a normal temperature");
+				bindImage("Resources/LaundrySymbols/Drying/Tumble/No_Tumble_Dry.svg.png", btn_do_not_tumble_dry, "Do not machine dry your clothing");
 
 				//Natural Dry
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Natural/Dry.svg.png"), btn_dry, "You can dry your clothing");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Natural/Line_Dry.svg.png"), btn_line_dry, "Line dry your clothing");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Natural/Dry_Flat.svg.png"), btn_dry_flat, "Dry your clothing on a flat surface");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Natural/Drip_Dry.svg.png"), btn_drip_dry, "Drip dry your clothing");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Natural/Dry_Shade.svg.png"), btn_dry_shade, "Dry your clothing in the shade");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Natural/Line_Dry_Shade.svg.png"), btn_line_dry_shade, "Line dry your clothing in the shade");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Natural/Dry_Flat_Shade.svg.png"), btn_dry_flat_shade, "Dry your clothing on a flat surface in the shade");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Drying/Natural/Drip_Dry_Shade.svg.png"), btn_drip_dry_shade, "Drip dry your clothing in the shade");
+				bindImage("Resources/LaundrySymbols/Drying/Natural/Dry.svg.png", btn_dry, "You can dry your clothing");
+				bindImage("Resources/LaundrySymbols/Drying/Natural/Line_Dry.svg.png", btn_line_dry, "Line dry your clothing");
+				bindImage("Resources/LaundrySymbols/Drying/Natural/Dry_Flat.svg.png", btn_dry_flat, "Dry your clothing on a flat surface");
+				bindImage("Resources/LaundrySymbols/Drying/Natural/Drip_Dry.svg.png", btn_drip_dry, "Drip dry your clothing");
+				bindImage("Resources/LaundrySymbols/Drying/Natural/Dry_Shade.svg.png", btn_dry_shade, "Dry your clothing in the shade");
+				bindImage("Resources/LaundrySymbols/Drying/Natural/Line_Dry_Shade.svg.png", btn_line_dry_shade, "Line dry your clothing in the shade");
+				bindImage("Resources/LaundrySymbols/Drying/Natural/Dry_Flat_Shade.svg.png", btn_dry_flat_shade, "Dry your clothing on a flat surface in the shade");
+				bindImage("Resources/LaundrySymbols/Drying/Natural/Drip_Dry_Shade.svg.png", btn_drip_dry_shade, "Drip dry your clothing in the shade");
 
 				//Iron
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Ironing/Iron.svg.png"), btn_iron, "You can iron your clothing");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Ironing/Iron_Low.svg.png"), btn_iron_low, "You can iron your clothing at the low setting");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Ironing/Iron_Medium.svg.png"), btn_iron_medium, "You can iron your clothing at the medium setting");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Ironing/Iron_High.svg.png"), btn_iron_high, "You can iron your clothing at the high setting");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Ironing/No_Iron.svg.png"), btn_do_not_iron, "Do not iron this clothing");
+				bindImage("Resources/LaundrySymbols/Ironing/Iron.svg.png", btn_iron, "You can iron your clothing");
+				bindImage("Resources/LaundrySymbols/Ironing/Iron_Low.svg.png", btn_iron_low, "You can iron your clothing at the low setting");
+				bindImage("Resources/LaundrySymbols/Ironing/Iron_Medium.svg.png", btn_iron_medium, "You can iron your clothing at the medium setting");
+				bindImage("Resources/LaundrySymbols/Ironing/Iron_High.svg.png", btn_iron_high, "You can iron your clothing at the high setting");
+				bindImage("Resources/LaundrySymbols/Ironing/No_Iron.svg.png", btn_do_not_iron, "Do not iron this clothing");
 
 				//Dry Cleaning
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/ProCleaning.svg.png"), btn_pro_cleaning, "You can professionally clean your clothing");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/HCS.svg.png"), btn_hcs_only, "When dry cleaning, use hydrocarbon solvents (HCS) only");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/HCS_Gentle.svg.png"), btn_hcs_gentle, "When dry cleaning, use hydrocarbon solvents (HCS) only. Make sure you are cleaning gently");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/HCS_Very_Gentle.svg.png"), btn_hcs_very_gentle, "When dry cleaning, use hydrocarbon solvents (HCS) only. Make sure you are cleaning very gently");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/PCE.svg.png"), btn_pce_only,"When dry cleaning, use perchloroethylene(PCE) only");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/PCE_Gentle.svg.png"), btn_pce_gentle,"When dry cleaning, use perchloroethylene(PCE) only. Make sure you are cleaning gently");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/PCE_Very_Gentle.svg.png"), btn_pce_very_gentle, "When dry cleaning, use perchloroethylene(PCE) only. Make sure you are cleaning very gently");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/No_Chemical_Cleaning.svg.png"), btn_no_chemical_clean, "Do not chemical clean this clothing");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/ProCleaning.svg.png", btn_pro_cleaning, "You can professionally clean your clothing");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/HCS.svg.png", btn_hcs_only, "When dry cleaning, use hydrocarbon solvents (HCS) only");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/HCS_Gentle.svg.png", btn_hcs_gentle, "When dry cleaning, use hydrocarbon solvents (HCS) only. Make sure you are cleaning gently");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/HCS_Very_Gentle.svg.png", btn_hcs_very_gentle, "When dry cleaning, use hydrocarbon solvents (HCS) only. Make sure you are cleaning very gently");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/PCE.svg.png", btn_pce_only,"When dry cleaning, use perchloroethylene(PCE) only");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/PCE_Gentle.svg.png", btn_pce_gentle,"When dry cleaning, use perchloroethylene(PCE) only. Make sure you are cleaning gently");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/PCE_Very_Gentle.svg.png", btn_pce_very_gentle, "When dry cleaning, use perchloroethylene(PCE) only. Make sure you are cleaning very gently");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/DryCleaning/No_Chemical_Cleaning.svg.png", btn_no_chemical_clean, "Do not chemical clean this clothing");
 
 				//Wet Cleaning
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/WetCleaning/Wet_Clean.svg.png"), btn_wet_clean, "Wet clean your clothing");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/WetCleaning/Wet_Clean_Gentle.svg.png"), btn_wet_clean_gentle,"Wet clean your clothing gently");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/WetCleaning/Wet_Clean_Very_Gentle.svg.png"), btn_wet_clean_very_gentle, "Wet clean your clothing very gently");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/ProfessionalCleaning/WetCleaning/No_Wet_Clean.svg.png"), btn_no_wet_clean, "Do not wet clean this clothing");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/WetCleaning/Wet_Clean.svg.png", btn_wet_clean, "Wet clean your clothing");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/WetCleaning/Wet_Clean_Gentle.svg.png", btn_wet_clean_gentle,"Wet clean your clothing gently");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/WetCleaning/Wet_Clean_Very_Gentle.svg.png", btn_wet_clean_very_gentle, "Wet clean your clothing very gently");
+				bindImage("Resources/LaundrySymbols/ProfessionalCleaning/WetCleaning/No_Wet_Clean.svg.png", btn_no_wet_clean, "Do not wet clean this clothing");
 
 				//Bleach
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Bleaching/Bleaching.svg.png"), btn_bleach, "You can bleach your clothing");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Bleaching/Bleaching_Cl.svg.png"), btn_bleach_cl, "Bleach your clothing with chlorine bleach");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Bleaching/Bleaching_No_Cl.svg.png"), btn_bleach_no_cl, "Bleach your clothing with non-chlorine bleach");
-				bindImage(new FileInputStream("Resources/LaundrySymbols/Bleaching/No_Bleaching.svg.png"), btn_do_not_bleach, "Do not bleach this clothing");
-	            
+				bindImage("Resources/LaundrySymbols/Bleaching/Bleaching.svg.png", btn_bleach, "You can bleach your clothing");
+				bindImage("Resources/LaundrySymbols/Bleaching/Bleaching_Cl.svg.png", btn_bleach_cl, "Bleach your clothing with chlorine bleach");
+				bindImage("Resources/LaundrySymbols/Bleaching/Bleaching_No_Cl.svg.png", btn_bleach_no_cl, "Bleach your clothing with non-chlorine bleach");
+				bindImage("Resources/LaundrySymbols/Bleaching/No_Bleaching.svg.png", btn_do_not_bleach, "Do not bleach this clothing");
+
 	        }catch(Exception e){
 	            System.out.println("Oh no");
 	        }
