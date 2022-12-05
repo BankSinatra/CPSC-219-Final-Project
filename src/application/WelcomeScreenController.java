19406904b0e71d1fe95c51353200e1909fb3eba4
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-05 16:01:27 -0700

    Added imports and variables to welcome screen controller

--- src/application/WelcomeScreenController.java
+++ src/application/WelcomeScreenController.java
@@ -1,5 +1,18 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class WelcomeScreenController {
+
+    @FXML
+    void switchToHomeScene(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToScene2(ActionEvent event) {
+
+    }
 
 }
