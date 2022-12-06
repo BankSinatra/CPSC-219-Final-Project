2cfa409d2d136c21d5d66f4b54ca17de7acfdb64
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-05 20:19:38 -0700

    The settings page navigates back to the main page

--- src/application/SettingsPageController.java
+++ src/application/SettingsPageController.java
@@ -1,10 +1,18 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class SettingsPageController {
 
+    @FXML
+    private Button btn_home;
+
     public void switchToHomeScene(ActionEvent actionEvent) {
+        Stage stage = (Stage) btn_home.getScene().getWindow();
+        stage.close();
     }
 
 }
