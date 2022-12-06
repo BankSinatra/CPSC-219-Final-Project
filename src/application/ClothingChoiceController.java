df4a32cf03f9069a52b0ee4be1406ea649109b86
ShannonPett <shannon.pett@ucalgary.ca>
2022-12-05 17:51:42 -0700

    Buttons clicked triggers action event.

--- src/application/ClothingChoiceController.java
+++ src/application/ClothingChoiceController.java
@@ -8,9 +8,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class ClothingChoiceController {
+	@FXML
+	private Button shoesButton;
 
     @FXML
     public void switchToHomeScene (ActionEvent event) throws IOException {
@@ -19,6 +22,31 @@ public class ClothingChoiceController {
    	 Scene scene = new Scene(root);
    	 stage.setScene(scene);
    	 stage.show();
+    }
+    
+    @FXML
+    void calculateHatSize(ActionEvent event) {
+    	System.out.println("HATSSS");
+    	boolean hats = true;
+    	
+    }
+    
+    @FXML
+    void calculateShoeSize (ActionEvent event) {
+    	System.out.println("SHOEEES");
+    	
+    }
+    
+    @FXML
+    void calculateTopsSize (ActionEvent event) {
+    	System.out.println("TOPSSS");
+    	
+    }
+    
+    @FXML 
+    void calculateBottomsSize (ActionEvent event) {
+    	System.out.println("BOTTOMSS");
+    	
     }
 
 }
