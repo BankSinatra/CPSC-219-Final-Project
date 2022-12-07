f3f8975faa058cb98a54e02f5b98266d345cb03a
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-06 21:26:38 -0700

    Added a Settings Controller so that each controller can hold and access the settings

---
+++ src/application/SettingsController.java
@@ -0,0 +1,14 @@
+package application;
+
+public class SettingsController {
+    private Settings settings;
+
+
+    public Settings getSettings() {
+        return settings;
+    }
+
+    public void setSettings(Settings settings) {
+        this.settings = settings;
+    }
+}
