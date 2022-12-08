f62b57fe8ccf08af54ee175f782a81955b05ba4a
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-08 13:35:51 -0700

    outfitCreatorCompletely works now

--- src/application/Temperature.java
+++ src/application/Temperature.java
@@ -2,24 +2,27 @@ package application;
 
 public class Temperature {
 	private double temperature;
+	private boolean celcius;
 	
 	
 	public Temperature(double temp){
+		celcius = true;
+		setTemperature(temp);
+	}
+
+	public Temperature(double temp, boolean c){
+		celcius  = c;
 		setTemperature(temp);
 	}
 	
-	
-	void converttofarenheit(double tempcelcius) {
-		setTemperature(tempcelcius);
-	}
-	
-	void converttocelcius(double tempfareinheit) {
-		setTemperature(tempfareinheit);
+	void convertToCelcius() {
+		if(!celcius){
+			setTemperature((Double)(temperature -32)*(5/9));
+		}
 	}
 	
 	public String toString() {
-	return ("the temperature you have saved is:" + getTemperature() + "degrees");
-	        
+		return ("the temperature you have saved is:" + getTemperature() + "degrees");
 	}
 
 
