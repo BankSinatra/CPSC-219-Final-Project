eda9759cc0196be31d7a4d5d164f2f16d1323848
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-09 12:13:41 -0700

    Implemented getHatSize function in HatScreenController

--- src/application/HatScreenController.java
+++ src/application/HatScreenController.java
@@ -1,5 +1,7 @@
 package application;
 
+import application.clothingModel.HeadCircumference;
+import application.clothingModel.InvalidSizeException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -24,11 +26,24 @@ public class HatScreenController {
     private TextField tf_circumference;
 
     public void getHatSize(ActionEvent actionEvent) {
+        Measurement m = new Measurement(measureUnit, Double.parseDouble(tf_circumference.getText()));
+        HeadCircumference myHead = new HeadCircumference(m);
+        try{
+            label_size_results.setText(String.valueOf(myHead.getHatSize(m)));
+            label_size_results.setVisible(true);
+            errorLabel.setVisible(false);
+            System.out.println(tf_circumference.getText());
+            settingsHolder.getSettings().getMeasurements().setHeadCircumference(Double.parseDouble(tf_circumference.getText()));
+            System.out.println(settingsHolder.getSettings().getMeasurements().getHeadCircumference());
+        } catch (InvalidSizeException e) {
+            errorLabel.setText(e.getMessage());
+            errorLabel.setVisible(true);
+        }
     }
 
     public void initialize(){
         label_unit.setText(settingsHolder.getSettings().getUnitString());
-        String text = String.valueOf(settingsHolder.getSettings().getMeasurements().getWaist()) ;
+        String text = String.valueOf(settingsHolder.getSettings().getMeasurements().getFootLength()) ;
         tf_circumference.setText(text);
     }
 }
