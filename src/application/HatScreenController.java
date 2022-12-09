d19d19b4556ea49ef03d746fd0b9f7f54ec856f4
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-09 11:50:26 -0700

    Created basic fields for the HatScreenController

--- src/application/HatScreenController.java
+++ src/application/HatScreenController.java
@@ -1,4 +1,34 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class HatScreenController {
+
+    private final SettingsHolder settingsHolder = SettingsHolder.getInstance();
+
+    private final MeasureUnit measureUnit = settingsHolder.getSettings().getUnitMeasurement();
+
+    @FXML
+    private Label label_size_results;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Label label_unit;
+
+    @FXML
+    private TextField tf_circumference;
+
+    public void getHatSize(ActionEvent actionEvent) {
+    }
+
+    public void initialize(){
+        label_unit.setText(settingsHolder.getSettings().getUnitString());
+        String text = String.valueOf(settingsHolder.getSettings().getMeasurements().getWaist()) ;
+        tf_circumference.setText(text);
+    }
 }
