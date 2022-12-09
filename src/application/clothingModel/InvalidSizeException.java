3cf6748d0efd4c62b922b0413c5c9f4f26481839
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-08 23:51:41 -0700

    Removed unneeded functions

--- src/application/clothingModel/InvalidSizeException.java
+++ src/application/clothingModel/InvalidSizeException.java
@@ -1,18 +1,7 @@
 package application.clothingModel;
 
 public class InvalidSizeException extends Exception{
-    InvalidSizeException(){
-    }
     InvalidSizeException(String msg){
         super(msg);
-    }
-    InvalidSizeException(Throwable cause){
-        super(cause);
-    }
-    InvalidSizeException(String msg, Throwable cause){
-        super(msg,cause);
-    }
-    InvalidSizeException(String msg, Throwable cause, boolean enableSuppression, boolean writableStackTrace){
-        super(msg, cause, enableSuppression, writableStackTrace);
     }
 }
