dd0b0917e00bb477a2440485bdf9bb85eeab500a
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-09 11:07:52 -0700

    Added the shoe screen fxml and controller

---
+++ src/application/ShoeScreenController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ShoeScreenController {
+    public TextField tf_foot;
+    public Label label_unit;
+    public Label errorLabel;
+    public Label label_size_results;
+
+    public void getShoeSize(ActionEvent actionEvent) {
+    }
+}
