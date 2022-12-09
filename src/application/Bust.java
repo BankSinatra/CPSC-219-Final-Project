c5c383142015b797b1a5e2f1e740b4977a5545a6
ShannonPett <shannon.pett@ucalgary.ca>
2022-12-09 11:41:02 -0700

    Java doc for bust

--- src/application/Bust.java
+++ src/application/Bust.java
@@ -1,11 +1,25 @@
 package application;
 
+/**
+ * This application will take the bust measurement and find the clothing size based on the 
+ * relevant clothing options for the measurement.
+ * @author CS219-user
+ *
+ */
 public class Bust {
 
+	/**
+	 * empty constructor
+	 */
 	public Bust() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
+	/**
+	 * Takes measurement and checks for which range size range it fits into and then assigns size.
+	 * @param measurement is the double value from user for bust size
+	 * @return string stating the size of clothing
+	 */
 	public String womensTops(double measurement) { // sizes xs - xxlarge, cm
 		String size = "";
 		if (78 <= measurement && measurement < 80.5) {
