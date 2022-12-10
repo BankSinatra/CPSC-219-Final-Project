2bcc5cdccc4fc71a7696c3b4c558911a3a5195b0
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-09 21:03:54 -0700

    Set up TopScreenMenController

--- src/application/TopScreenMenController.java
+++ src/application/TopScreenMenController.java
@@ -1,4 +1,15 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class TopScreenMenController {
+    public Label label_size_results;
+    public Label errorLabel;
+    public Label label_unit;
+    public TextField tf_chest;
+
+    public void getMensTopSize(ActionEvent actionEvent) {
+    }
 }
