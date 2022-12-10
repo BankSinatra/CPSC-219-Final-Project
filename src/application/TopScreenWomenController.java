22338c4549a148da75762b526ee471a45e738383
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-09 20:24:11 -0700

    Added the top screen for women and its controller

---
+++ src/application/TopScreenWomenController.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class TopScreenWomenController {
+
+
+    public Label label_size_results;
+    public Label hips_errorLabel;
+    public Label label_unit_2;
+    public TextField tf_hips;
+    public Label waist_errorLabel;
+    public Label label_unit;
+    public TextField tf_waist;
+    public Label bust_errorLabel;
+    public Label label_unit_3;
+    public TextField tf_bust;
+
+    public void getWomenTopSize(ActionEvent actionEvent) {
+    }
+}
