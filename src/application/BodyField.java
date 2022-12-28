edcf31008c6c5087dce6bd0ceb7bc4d96d330140
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-28 09:30:13 -0700

    Added more instance variables and methods to the BodyField class

--- src/application/BodyField.java
+++ src/application/BodyField.java
@@ -8,15 +8,17 @@ import javafx.scene.text.Font;
 
 import java.util.Locale;
 
-public class BodyField extends VBox {
+public class BodyField extends VBox{
     MeasureUnit unit;
     Label errorTF = new Label();
     TextField textField = new TextField();
     Label unitLabel = new Label();
     private HBox hBox = new HBox();
+    private BodyPart bodyPart;
 
-    BodyField(String name, MeasureUnit unit){
+    BodyField(String name, MeasureUnit unit, BodyPart bodyPart){
         this.unit = unit;
+        this.bodyPart = bodyPart;
 
         TextField nameTF = new TextField(name);
         nameTF.setFont(Font.font("System Bold", 18));
@@ -29,5 +31,9 @@ public class BodyField extends VBox {
 
         errorTF.setVisible(false);
         this.getChildren().add(errorTF);
+    }
+
+    public TextField getTextField(){
+        return textField;
     }
 }
