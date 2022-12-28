1f75d01955e1f2e8f3e29d6de1c013669ffd0c2b
Eyram Ekpe <51966182+BankSinatra@users.noreply.github.com>
2022-12-28 10:52:10 -0700

    Added a results label to the Size dialog. No more ArrayList as weell

--- src/application/SizeDialog.java
+++ src/application/SizeDialog.java
@@ -1,17 +1,19 @@
 package application;
 
+import application.clothingModel.InvalidSizeException;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SizeDialog extends VBox {
-    BodyPart[] bodyParts;
-    ClothingType clothingType;
-    boolean isMale;
-    MeasureUnit units;
-    ArrayList<BodyField> bodyFields;
+    private BodyPart[] bodyParts;
+    private ClothingType clothingType;
+    private boolean isMale;
+    private MeasureUnit units;
+    HashMap<BodyField, Measurement> bodyFields = new HashMap<BodyField, Measurement>();
+    private Label resultsLabel = new Label();
 
     SizeDialog(ClothingType clothingType, boolean isMale, MeasureUnit units, BodyPart[] bodyParts1){
         this.bodyParts = bodyParts1;
@@ -20,7 +22,6 @@ public class SizeDialog extends VBox {
         this.units = units;
         setTextViews(bodyParts);
 
-        Label resultsLabel = new Label();
         resultsLabel.setVisible(false);
         this.getChildren().add(resultsLabel);
 
@@ -32,18 +33,36 @@ public class SizeDialog extends VBox {
     private void setTextViews(BodyPart[] bodyParts){
         for(BodyPart bodyPart : bodyParts){
             BodyField bf = new BodyField(bodyPart.toString(), units, bodyPart);
-            bodyFields.add(bf);
+            bodyFields.put(bf, new Measurement(units));
             this.getChildren().add(bf);
         }
     }
 
     private void calculateSize(){
-        for(BodyField bf : bodyFields){
-            Measurement measurement = new Measurement(units, Double.parseDouble(bf.getTextField().getText()));
+        int size = 0;
+        this.resultsLabel.setVisible(false);
+        for(BodyField bf : bodyFields.keySet()){
+            try {
+                bodyFields.get(bf).setValue(Double.parseDouble(bf.getTextField().getText())); //Set the value of the value based on the text of the key
+                bf.getBodyPart().setMeasurement(bodyFields.get(bf));
+                BodyPart bodyPart = bf.getBodyPart();
+
+                int bodyPartMeasurement = bodyPart.getMeasurement(clothingType);
+                if(bodyPartMeasurement > size){
+                    size = bodyPartMeasurement;
+                }
+                bf.getErrorLabel().setVisible(false);
+            }catch (InvalidSizeException e) {
+                bf.getErrorLabel().setText(e.getMessage());
+                bf.getErrorLabel().setVisible(true);
+            }catch (NumberFormatException e){
+                bf.getErrorLabel().setText("Please enter a number");
+                bf.getErrorLabel().setVisible(true);
+            }
 
         }
+        this.resultsLabel.setText(String.valueOf(size));
+        this.resultsLabel.setVisible(true);
     }
-
-
 
 }
